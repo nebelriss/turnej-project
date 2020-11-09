64e436aa882e0d07e4ec054047cb13c07e954ad4
Michel <git@akiramail.com>
2020-11-09 21:38:50 +0100

    some changes

--- start.sh
+++ start.sh
@@ -5,27 +5,27 @@ echo "starting to clone repositories"
 # Download all parts
 # db:
 echo "getting database"
-git clone https://github.com/nebelriss/turnej-db.git db > /dev/null
+git clone https://github.com/nebelriss/turnej-db.git db
 echo "done"
 
 # spielplan
 echo "getting lib/spielplan"
-git clone https://github.com/nebelriss/turnej-spielplan.git lib/spielplan > /dev/null
+git clone https://github.com/nebelriss/turnej-spielplan.git libs/spielplan
 echo "done"
 
 # server:
 echo "getting server"
-git clone https://github.com/nebelriss/turnej-server.git server > /dev/null
+git clone https://github.com/nebelriss/turnej-server.git server
 echo "done"
 
 # gui:
 echo "getting web-ui"
-git clone https://github.com/nebelriss/turnej.git web-ui > /dev/null
+git clone https://github.com/nebelriss/turnej.git web-ui
 echo "done"
 
 # api-docs
 echo "getting api-docs"
-git clone https://github.com/nebelriss/turnej-api-docs.git api-docs > /dev/null
+git clone https://github.com/nebelriss/turnej-api-docs.git api-docs
 echo "done"
 
 echo "All set up, enjoy..."
